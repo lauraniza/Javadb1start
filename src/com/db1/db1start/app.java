e0f7055f3d7fa93b576d1c52f33031c0fef93c31
Laura Niza <laurapegininiza@hotmail.com>
2019-11-11 17:10:56 -0300

    Fim primeira aula

--- src/com/db1/db1start/app.java
+++ src/com/db1/db1start/app.java
@@ -4,52 +4,55 @@ import java.util.Scanner;
 
 public class app {
     public static void main (String[] args){
-        Scanner scan = new Scanner(System.in);
+       Scanner scan = new Scanner(System.in);
         //exercicio 1 de inteiro
-        Integer numero1, numero2;
         System.out.println("Digite um número:");
-        numero1 = scan.next();
+        Integer numero1 = Integer.valueOf(scan.next());
         System.out.println("Digite outro número:");
-        numero2 = scan.next();
+        Integer numero2 = Integer.valueOf(scan.next());
         System.out.println(numero1 + numero2);
 
         //exercicio 2 de inteiro
         System.out.println("Digite um número:");
-        numero1 = scan.next();
+        numero1 = Integer.valueOf(scan.next());
         System.out.println("Digite outro número:");
-        numero2 = scan.next();
+        numero2 = Integer.valueOf(scan.next());
         System.out.println(numero1 - numero2);
-
+*/
         //exercicio 1 de string
         System.out.printf("Informe uma frase em caracteres minúsculos:\n");
         String text = scan.nextLine();
         System.out.println(text.toUpperCase());
 
-        //exercicio 2 string
+     //exercicio 2 string
         System.out.printf("Informe uma frasecom caracteres maiúsculo e minúsculos:\n");
         text = scan.nextLine();
         System.out.println(text.toLowerCase());
 
         //exercicio 1 matematica
-        Double numero01, numero02;
-        System.out.println("Digite um número real:");
-        numero01 = scan.next();
-        System.out.println("Digite outro número real:");
-        numero02 = scan.next();
-        if(numero01 > numero02) System.out.println("O menor número é ", numero02);
-        else if (numero02 > numero01){
-            System.out.println("O menor número é ", numero01);
-        }else {
-            System.out.println("Os números tem o mesmo valor.");
+        System.out.println("Digite um valor:");
+        Double valor1 = scan.nextDouble();
+        System.out.println("Digite outro valor:");
+        Double valor2 = scan.nextDouble();
+        if(valor1 > valor2) {
+            System.out.println("O menor valor é o 2");
+        } else if (valor2 > valor1){
+            System.out.println("O menor valor é o 1");
         }
 
         //exercicio 2 de matematica
-        System.out.println("Digite um número real:");
-        numero01 = scan.next();
-        System.out.println("Digite outro número real:");
-        numero02 = scan.next();
-        System.out.println("Digite outro número real:");
-        Double numero03;
-        numero03= scan.next();
+        System.out.println("Digite um valor:");
+        valor1 = scan.nextDouble();
+        System.out.println("Digite outro valor:");
+        valor2 = scan.nextDouble();
+        System.out.println("Digite outro valor:");
+        Double valor3= scan.nextDouble();
+        if(valor1 < valor2 && valor1< valor3){
+            System.out.println("Menor valor é o 1");
+        }else if (valor2 < valor3){
+            System.out.println("Menor valor é o 2");
+        }else {
+            System.out.println("Menor valor é o 3");
+        }
   }
 }
