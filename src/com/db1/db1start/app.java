c273229539e78b9008ad8ff6d4347ae26a75200f
Laura Niza <laurapegininiza@hotmail.com>
2019-11-12 13:43:02 -0300

    separação dos exercícios

--- src/com/db1/db1start/app.java
+++ src/com/db1/db1start/app.java
@@ -3,56 +3,65 @@ package com.db1.db1start;
 import java.util.Scanner;
 
 public class app {
-    public static void main (String[] args){
-       Scanner scan = new Scanner(System.in);
-        //exercicio 1 de inteiro
+    public static void ex1Numero(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Digite um número:");
         Integer numero1 = Integer.valueOf(scan.next());
         System.out.println("Digite outro número:");
         Integer numero2 = Integer.valueOf(scan.next());
         System.out.println(numero1 + numero2);
+    }
 
-        //exercicio 2 de inteiro
+    public static void ex2Numero(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Digite um número:");
-        numero1 = Integer.valueOf(scan.next());
+        Integer numero1 = Integer.valueOf(scan.next());
         System.out.println("Digite outro número:");
-        numero2 = Integer.valueOf(scan.next());
+        Integer numero2 = Integer.valueOf(scan.next());
         System.out.println(numero1 - numero2);
-*/
-        //exercicio 1 de string
+    }
+
+    public static void ex1String(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.printf("Informe uma frase em caracteres minúsculos:\n");
         String text = scan.nextLine();
         System.out.println(text.toUpperCase());
+    }
 
-     //exercicio 2 string
+    public static void ex2String(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.printf("Informe uma frasecom caracteres maiúsculo e minúsculos:\n");
-        text = scan.nextLine();
+        String text = scan.nextLine();
         System.out.println(text.toLowerCase());
+    }
 
-        //exercicio 1 matematica
+    public static void ex1Matematica(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Digite um valor:");
         Double valor1 = scan.nextDouble();
         System.out.println("Digite outro valor:");
         Double valor2 = scan.nextDouble();
-        if(valor1 > valor2) {
+        if (valor1 > valor2) {
             System.out.println("O menor valor é o 2");
-        } else if (valor2 > valor1){
+        } else if (valor2 > valor1) {
             System.out.println("O menor valor é o 1");
         }
+    }
 
-        //exercicio 2 de matematica
+    public static void ex2Matematica(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Digite um valor:");
-        valor1 = scan.nextDouble();
+        Double valor1 = scan.nextDouble();
         System.out.println("Digite outro valor:");
-        valor2 = scan.nextDouble();
+        Double valor2 = scan.nextDouble();
         System.out.println("Digite outro valor:");
-        Double valor3= scan.nextDouble();
-        if(valor1 < valor2 && valor1< valor3){
+        Double valor3 = scan.nextDouble();
+        if (valor1 < valor2 && valor1 < valor3) {
             System.out.println("Menor valor é o 1");
-        }else if (valor2 < valor3){
+        } else if (valor2 < valor3) {
             System.out.println("Menor valor é o 2");
-        }else {
+        } else {
             System.out.println("Menor valor é o 3");
         }
-  }
+    }
 }
