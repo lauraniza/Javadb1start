89750ffa76e9f1404b64aa505f7dccf45f7e5154
Laura Niza <laurapegininiza@hotmail.com>
2019-11-18 15:19:14 -0300

    correção dos exercícios de lista - complementar main

--- src/com/db1/db1start/ExerciciosRobson.java
+++ src/com/db1/db1start/ExerciciosRobson.java
@@ -1,11 +1,14 @@
 package com.db1.db1start;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.*;
 
 public class ExerciciosRobson {
+
+    public static void main(){
+
+    }
 
     //Método que retorne os nomes das cores que você mais gosta
     public List<String> coresFavoritas (){
@@ -20,35 +23,46 @@ public class ExerciciosRobson {
     }
 
     //Método que receba 3 String, adicione todos em uma lista e remova a segunda posição
-    public List<String> inserir3excluirPosicao2(){
+    public static void inserir3excluirPosicao2(){
         List<String> cidades = new ArrayList<>();
         cidades.addAll(Arrays.asList("Maringá","Marialva","Mandaguaçu"));
         cidades.remove(1);
-        return cidades;
     }
 
     //Método que imprima a lista de cores do primeiro método
-    public void imprimirCores (){
-        System.out.println(coresFavoritas());
+    public static void imprimirCores (){
+        coresFavoritas().forEach(System.out::println);
 }
 
     //Método que imprima as cores do primeiro método em ordem alfabética
-    public List<String> coresOrdemAlfabetica (){
-        return coresFavoritas(Collections.sort();
+    public static void coresOrdemAlfabetica (){
+        List<String> cores = coresFavoritas();
+        Collections.sort(cores);
+        cores().forEach(System.out::println);
     }
 
     //Método que receba uma lista das cores que você mais gosta e o nome de uma cor a ser removida
-    public List<String> removerUmaCor (){
-        List<String> coresRemocao = new ArrayList<>();
-        coresRemocao = coresFavoritas();
+    public static void removerUmaCor (){
+        List<String> coresRemocao = coresFavoritas();
         coresRemocao.remove("Amarelo");
-        return coresRemocao;
     }
 
     //Método que receba a lista de cores que você gosta e imprima em ordem decrescente (alfabética)
-    public List<String> coresDecrescente (){
-
+    public static void coresDecrescente (){
+        List<String> cores = coresFavoritas();
+        Collections.sort(cores, Collections.reverseOrder());
+        cores().forEach(System.out::println);
     }
 
     //Método que receba uma lista de números e retorne um mapa com listas de números pares e impares
+    public Map<String, List<Integer>> SepararParesImpares (List<Integer> numeros){
+        Map<String, List<Integer>> mapa = new HashMap<>();
+        mapa.put("PAR", new ArrayList<>());
+        mapa.put("ÍMPAR", new ArrayList<>());
+        numeros.forEach(n ->{
+            String chave = n % 2 == 0 ? "PAR" : "IMPAR";
+            mapa.get(chave).add(n);
+        });
+        return mapa;
+    }
 }
